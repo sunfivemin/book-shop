b1ab22aac335bda9d4b7a869c9993d8a876340c0
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-05-14 16:40:53 +0900

    🎨 라우터에서 분리해서 컨트롤러 모듈화

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
-const { StatusCodes } = require('http-status-codes');
+
+const join = require('../controller/UserController');
 
 router.use(express.json());
 
-router.post('/join', (req, res) => {
-  const { email, password } = req.body;
-
-  let sql = 'INSERT INTO user (email, password) VALUES (?,?)';
-  let values = [email, password];
-
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.CREATED).json(results);
-  });
-});
+router.post('/join', join);
 
 router.post('/login', (req, res) => {
   res.json('로그인');
