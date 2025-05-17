8f04b1ff861cddb06f055b98d9f4938433623859
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-05-17 23:38:05 +0900

    🎨 카테고리별, 신간, 도서 목록 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,23 +1,49 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
+// 카테고리별, 신간 여부 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  // 요약된 전체 도서 리스트
+  let { category_id, news } = req.query;
   let sql = 'SELECT * FROM books';
-  conn.query(sql, (err, results) => {
+  let values = [];
+
+  // 1. 카테고리와 신간 모두 포함된 경우
+  if (category_id && news) {
+    sql +=
+      ' WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+    values = [category_id, news];
+
+    // 2. 카테고리만 있는 경우
+  } else if (category_id) {
+    sql += ' WHERE category_id=?';
+    values = [category_id];
+
+    // 3. 신간만 있는 경우
+  } else if (news) {
+    sql +=
+      ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+    values = [news];
+  }
+
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
+
     return res.status(StatusCodes.OK).json(results);
   });
 };
 
+// 도서 상세 조회 API
 const bookDetail = (req, res) => {
-  let { id } = req.params;
+  const { id } = req.params;
+  const sql = `
+    SELECT * FROM books 
+    LEFT JOIN category ON books.category_id = category.id 
+    WHERE books.id = ?`;
 
-  let sql = 'SELECT * FROM books WHERE id=?';
-  conn.query(sql, id, (err, results) => {
+  conn.query(sql, [id], (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
--- data.sql
+++ data.sql
@@ -9,3 +9,29 @@ VALUES ("효녀 심청", "종이책", "9", "심청아..", "공양미 삼백석..
 
 INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
 VALUES ("혹부리 영감", "ebook", "10", "노래 주머니..", "혹 두개 되버림..", "김영감", 100, "목차입니다.", 20000, "2025-05-05");
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("콩쥐 팥쥐", 4, 0, "ebook", 4, "콩팥..", "콩심은데 콩나고..", "김콩팥", 100, "목차입니다.", 20000, "2023-12-07");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("용궁에 간 토끼", 5, 1, "종이책", 5, "깡충..", "용왕님 하이..", "김거북", 100, "목차입니다.", 20000, "2023-10-01");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("해님달님", 15, 2, "ebook", 6, "동앗줄..", "황금 동앗줄..!", "김해님", 100, "목차입니다.", 20000, "2023-07-16");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("장화홍련전", 80, 0, "ebook", 7, "기억이 안나요..", "장화와 홍련이?..", "김장화", 100, "목차입니다.", 20000, "2025-04-25");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("견우와 직녀", 8, 1, "ebook", 8, "오작교!!", "칠월 칠석!!", "김다리", 100, "목차입니다.", 20000, "2025-05-02");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("효녀 심청", 12, 0, "종이책", 9, "심청아..", "공양미 삼백석..", "김심청", 100, "목차입니다.", 20000, "2025-05-01");
+
+INSERT INTO books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("혹부리 영감", 22, 2, "ebook", 10, "노래 주머니..", "혹 두개 되버림..", "김영감", 100, "목차입니다.", 20000, "2025-05-05");
+SELECT * FROM books
+LEFT JOIN category ON books.category_id = category.id;
+
+SELECT * FROM books 
+LEFT JOIN category ON books.category_id = category.id
+WHERE books.id = 1;
