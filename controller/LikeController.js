0b172c12cea31e6bc7acfc851a562fcd0e965e6e
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-05-19 20:42:05 +0900

    🎨 DELETE 요청 수정

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -21,7 +21,7 @@ const removeLike = (req, res) => {
   const { id } = req.params; // book_id
   const { user_id } = req.body;
 
-  let sql = 'INSERT INTO likes (user_id, liked_book_id) VALUES (?,?)';
+  let sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?';
   let values = [user_id, id];
   conn.query(sql, values, (err, results) => {
     if (err) {
