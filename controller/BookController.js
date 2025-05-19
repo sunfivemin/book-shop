b3ee381a743c72ae473a74213268c627836e82db
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-05-19 17:07:42 +0900

    🎨 도서 목록 조회 페이징 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,17 +1,23 @@
+const { response } = require('express');
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 // 카테고리별, 신간 여부 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  let { category_id, news } = req.query;
+  let { category_id, news, limit, currentPage } = req.query;
+  // limit : page 당 도서수               ex. 3
+  // currentPage : 현재 및 페이지          ex. 1, 2, 3 ...
+  // offset : limit * (currentPage-1)   ex. 0, 3, 6 ...
+
+  let offset = limit * (currentPage - 1);
   let sql = 'SELECT * FROM books';
-  let values = [];
+  let values = [parseInt(limit), offset];
 
   // 1. 카테고리와 신간 모두 포함된 경우
   if (category_id && news) {
     sql +=
       ' WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-    values = [category_id, news];
+    values = [category_id];
 
     // 2. 카테고리만 있는 경우
   } else if (category_id) {
@@ -22,8 +28,10 @@ const allBooks = (req, res) => {
   } else if (news) {
     sql +=
       ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-    values = [news];
   }
+
+  sql += ' LIMIT ? OFFSET ? ';
+  values.push(parseInt(limit), offset);
 
   conn.query(sql, values, (err, results) => {
     if (err) {
