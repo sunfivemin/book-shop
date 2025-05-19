8253e9fc0254ffd55689727b70529ae3afdc34f6
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-05-19 20:33:28 +0900

    🎨 좋아요 추가, 삭제

--- routes/likes.js
+++ routes/likes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { addLike, removeLike } = require('../controller/LikeController');
 
 router.use(express.json());
 
-router.post('/:id', (req, res) => {
-  res.json('좋아요 추가');
-});
+router.post('/:id', addLike);
 
-router.delete('/:id', (req, res) => {
-  res.json('좋아요 삭제');
-});
+router.delete('/:id', removeLike);
 
 module.exports = router;
